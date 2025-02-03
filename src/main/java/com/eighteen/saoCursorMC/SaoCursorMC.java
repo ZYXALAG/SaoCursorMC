bd58bf3682396c0518196d27bbea20d188663473
ZYXALAG <adrien.leschaeve@gmail.com>
2025-02-03 20:30:00 +0100

    base files

---
+++ src/main/java/com/eighteen/saoCursorMC/SaoCursorMC.java
@@ -0,0 +1,17 @@
+package com.eighteen.saoCursorMC;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SaoCursorMC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
